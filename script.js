923ce7b731c2c3ef0bd316847e42ee50cef859cc
Amanda Case <a.cath.case@gmail.com>
2018-06-12 19:56:42 -0400

    review function adds reviews to storage

---
+++ script.js
@@ -0,0 +1,13 @@
+var reviewStorage = [];
+
+var review = {
+    gameplay: "",
+    visuals: "",
+    story: "",
+    comments: "",
+    overallScore: ""
+}
+
+function addReview() {
+    reviewStorage.push(review);
+}
