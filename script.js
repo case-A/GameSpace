898b57d7f335a1adeebbacf9017164036290db56
Amanda Case <a.cath.case@gmail.com>
2018-06-12 20:33:39 -0400

    adds reviews using constructor function

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 var reviewStorage = [];
 
-var review = {
-    gameplay: "",
-    visuals: "",
-    story: "",
-    comments: "",
-    overallScore: ""
+function Review(gameplayScore, visualsScore, storyScore, comments, overallScore){
+    this.gameplayScore = gameplayScore,
+    this.visualsScore = visualsScore,
+    this.storyScore = storyScore,
+    this.comments = comments,
+    this.overallScore = overallScore
 }
 
-function addReview() {
-    reviewStorage.push(review);
-}
+
+//It should have a function to add gameplayScore property value (1-5)
+//It should have a function to add visualScore property value (1-5)
+//It should have a function to add storyScore property value (1-5)
+//It should have a function to add comments
+
+
+//It should have a function that totals and averages the 'Scores'
