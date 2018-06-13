bdfd589ee18ce4ac7c40ba8eadde6e66a2b3913a
Amanda Case <a.cath.case@gmail.com>
2018-06-12 21:00:36 -0400

    reviews are now stored and accessed via reviews variable

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-var reviewStorage = [];
+var reviews = [];
 
-function Review(gameplayScore, visualsScore, storyScore, comments, overallScore){
-    this.gameplayScore = gameplayScore,
-    this.visualsScore = visualsScore,
-    this.storyScore = storyScore,
-    this.comments = comments,
-    this.overallScore = overallScore
+function addReview(gameplayScore, visualScore, storyScore, overallScore, comment){
+    reviews.push({
+        "gameplayScore": gameplayScore,
+        "visualScore": visualScore,
+        "storyScore": storyScore,
+        "overallScore": overallScore,
+        "comment": comment
+    })
 }
-
-
-//It should have a function to add gameplayScore property value (1-5)
-//It should have a function to add visualScore property value (1-5)
-//It should have a function to add storyScore property value (1-5)
-//It should have a function to add comments
-
-
-//It should have a function that totals and averages the 'Scores'
