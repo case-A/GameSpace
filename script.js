b3420871e02bc156aefd53aeef3f03be780efdf3
Amanda Case <a.cath.case@gmail.com>
2018-06-14 12:59:02 -0400

    has a function to edit reviews

--- script.js
+++ script.js
@@ -7,5 +7,22 @@ function addReview(gameplayScore, visualScore, storyScore, overallScore, comment
         "storyScore": storyScore,
         "overallScore": overallScore,
         "comment": comment
-    })
+    });
+    console.log(reviews);
 }
+
+//Overall score should automatically average other scores
+    // total of scores/ 3
+    //gameplayScore + visualScore + storyScore/ 3
+//It should have a function to edit a review
+    //need to access review index and individual review property and then property keys value.
+        function editReview(reviewPosition, newGameplayScore, newVisualScore, newStoryScore, newCommentText){
+            reviews[reviewPosition].gameplayScore = newGameplayScore;
+            reviews[reviewPosition].visualScore = newVisualScore;
+            reviews[reviewPosition].storyScore = newStoryScore;
+            reviews[reviewPosition].comment = newCommentText;
+            console.log(reviews);
+        }
+
+// It should have a function to remove a review
+//It should have a function that averages total overallScore
