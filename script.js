6fa74b28e1ab2aa8f387758d8831ec9dcf5363e5
Amanda Case <a.cath.case@gmail.com>
2018-06-27 19:03:02 -0400

    now has a function that averages the scoreAverage of all reviews

--- script.js
+++ script.js
@@ -5,23 +5,29 @@ function addReview(gameplayScore, visualScore, storyScore, comment){
         "gameplayScore": gameplayScore,
         "visualScore": visualScore,
         "storyScore": storyScore,
-        "overallScore": Math.floor((gameplayScore + visualScore + storyScore)/3),
+        "scoreAverage": Math.floor((gameplayScore + visualScore + storyScore)/3),
         "comment": comment
     });
     console.log(reviews);
 }
 
 
-//It should have a function that averages total overallScore    
-    function averageScores(reviewPosition) {
-        var gameplayScoreValue = reviews[reviewPosition].gameplayScore;
-        var visualScoreValue = reviews[reviewPosition].visualScore;
-        var storyScoreValue = reviews[reviewPosition].storyScore;
-        var totalScore = gameplayScoreValue + visualScoreValue + storyScoreValue;
-
-        var overallScore = Math.floor(totalScore /3);
-        return overallScore;
+//It should have a function that averages total overallScore of reviews
+function overallScore() {
+    debugger;
+    var overallScoresArray = []; 
+    for(var i = 0; i < reviews.length; i++) {
+       overallScoresArray.push(reviews[i].scoreAverage); 
+       };
+    function getSum(total, num) {
+        return total + num;
     }
+    var totalScores = overallScoresArray.reduce(getSum, 0);
+    var overallScore = totalScores /reviews.length;
+    return overallScore;
+} 
+    
+
 //It should have a function to edit a review
     //need to access review index and individual review property and then property keys value.
         function editReview(reviewPosition, newGameplayScore, newVisualScore, newStoryScore, newCommentText){
